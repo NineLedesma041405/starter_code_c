d4597eb3ca3303be921becff3dfa0e535f96f392
NineLedesma041405 <jeannine_ledesma@dlsu.edu.ph>
2026-02-08 20:32:24 +0800

    first commit

---
+++ starter_code_c/main.c
@@ -0,0 +1,123 @@
+#include "record.c"
+#include "sortingalgorithms.c"
+#include "filereader.c"
+#include "timer.c"
+#include <stdio.h>
+#include <string.h>
+
+void writeFile (Record *records, int count) {
+
+    FILE *outputFile = fopen("sorted.txt", "w");;
+
+    if (outputFile == NULL) 
+        return;
+
+
+    fprintf(outputFile, "%d\n", count);
+
+    for (int i = 0; i < count; i++) {
+        fprintf(outputFile, "%d %s\n", records[i].idNumber, records[i].name);
+    }
+
+    fclose(outputFile);
+    printf("Successfully saved to sorted.txt\n");
+
+}
+
+int main()
+{
+    int count, recchoice, sortchoice;
+    char filename[500];
+
+    do {
+
+        printf("Select the record to sort:\n");
+        printf("1. almostsorted.txt\n");
+        printf("2. random100.txt\n");
+        printf("3. random25000.txt\n");
+        printf("4. random50000.txt\n");
+        printf("5. random75000.txt\n");
+        printf("6. random100000.txt\n");
+        printf("7. totallyreversed.txt\n");
+        
+        printf("Input number (1-7): ");
+        scanf("%d", &recchoice);
+
+        switch (recchoice) {
+            case 1:
+                strcpy(filename, "data/almostsorted.txt"); 
+                break;
+            case 2:
+                strcpy(filename, "data/random100.txt");    
+                break;
+            case 3:
+                strcpy(filename, "data/random25000.txt");  
+                break;
+            case 4: 
+                strcpy(filename, "data/random50000.txt");  
+                break;
+            case 5:
+                strcpy(filename, "data/random75000.txt");  
+                break;
+            case 6:
+                strcpy(filename, "data/random100000.txt"); 
+                break;
+            case 7: 
+                strcpy(filename, "data/totallyreversed.txt"); 
+                break;
+            default:
+                printf("Invalid choice.");
+                recchoice = 0;
+        }
+
+    } while (recchoice < 1 || recchoice > 7);
+
+    FILE *inputFile = fopen(filename, "r");
+
+    if (inputFile == NULL) {
+        printf("Error: Could not open %s.\n", filename);
+        return 1;
+    }
+    fscanf(inputFile, "%d", &count);
+    fclose(inputFile);
+
+    static Record myRecords[100001];
+
+    readFile(myRecords, filename);
+
+    do {
+
+        printf("Select method of sorting:\n");
+        printf("1. Insertion Sort\n");
+        printf("2. Selection Sort\n");
+        printf("3. Merge Sort\n");
+        printf("4. Quick Sort\n");
+
+        printf("Input number (1-7): ");
+        scanf("%d", &sortchoice);
+
+        switch (sortchoice) {
+            case 1:
+                // add part
+                break;
+            case 2:
+                selectionSort(myRecords, count);  
+                writeFile(myRecords, count);
+                break;
+            case 3:
+                // add part 
+                break;
+            case 4: 
+                quickSort (myRecords, 0, count - 1);
+                writeFile(myRecords, count);
+                break;
+            default:
+                printf("Invalid choice.");
+                sortchoice = 0;
+        }
+
+    } while (sortchoice < 1 || sortchoice > 4);
+
+
+    return 0;
+}
---
+++ starter_code_c/sortingalgorithms.c
@@ -0,0 +1,105 @@
+#ifndef SORTINGALGORITHMS_C
+#define SORTINGALGORITHMS_C
+
+#include <stdio.h>
+#include "record.c"
+
+/*
+* You may declare additional variables and helper functions
+* as needed by the sorting algorithms here.
+*/
+
+// for quick sort
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// for quick sort
+int partition(Record *arr, int low, int high) {
+
+    int pivot = arr[high].idNumber; 
+    int i = (low - 1); 
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j].idNumber < pivot) {
+            i++;
+
+            Record temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    Record temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+        
+    return (i + 1);
+}
+
+
+void insertionSort(Record *arr, int n)
+{
+    // TODO: Implement this sorting algorithm here.
+
+
+
+
+
+
+}
+
+void selectionSort(Record *arr, int n)
+{
+    // TODO: Implement this sorting algorithm here.
+
+
+    for (int i = 0; i < n - 1; i++) {
+
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j].idNumber < arr[min_idx].idNumber) {
+                min_idx = j;
+            }
+        }    
+
+        Record temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+
+    }
+}
+
+void mergeSort(Record *arr, int p, int r)
+{
+    // TODO: Implement this sorting algorithm here.
+
+
+
+
+
+
+}
+
+/*
+* Define AT LEAST ONE more sorting algorithm here, apart from the
+* ones given above. Make sure that the method accepts an array of
+* record structures.
+*/
+
+void quickSort (Record *arr, int low, int high) {
+
+    if (low < high) {
+
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+
+}
+
+
+#endif
